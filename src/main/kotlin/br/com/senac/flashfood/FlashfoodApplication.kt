a3e9624dd744e58610aca0839cec229c8dc46089
Rafan <rafanarbutis@outlook.com>
2021-05-05 20:56:02 -0300

    Initial commit

---
+++ src/main/kotlin/br/com/senac/flashfood/FlashfoodApplication.kt
@@ -0,0 +1,11 @@
+package br.com.senac.flashfood
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FlashfoodApplication
+
+fun main(args: Array<String>) {
+	runApplication<FlashfoodApplication>(*args)
+}
